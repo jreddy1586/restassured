fea5f9f2f0dc9445001c8b0b9ad387ab1173c25b
jreddy1586 <95558343+jreddy1586@users.noreply.github.com>
2021-12-05 00:52:31 -0500

    creating new project

---
+++ src/main/java/com/getprojectapi/restservice/GetProjectApi.java
@@ -0,0 +1,15 @@
+package com.getprojectapi.restservice;
+
+
+import org.testng.annotations.Test;
+
+public class GetProjectApi {
+
+		@Test
+		public void healthCheckTest() {
+
+			
+			
+		}
+		
+}
